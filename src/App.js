7ca96156293e82c520b34352c4713ae9f315a81c
Hernan-Alonso <hernanmalonso@gmail.com>
2020-12-03 21:03:09 -0300

    React Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 function App() {
+  //crear listado de productos
+  // eslint-disable-next-line
+  const [productos, guardarProductos] = useState([
+    {id: 1, nombre: 'Camisa reactJS', precio: 50},
+    {id: 2, nombre: 'Camisa VueJS', precio: 40},
+    {id: 3, nombre: 'Camisa NodeJS', precio: 30},
+    {id: 4, nombre: 'Camisa Angular', precio: 20},
+  ]);
+  //State para el carrito de compras
+  const [carrito, agregarProducto] = useState([]);
   //obtener año actual
   const fecha = new Date().getFullYear();
   return (
     <Fragment>
       <Header 
         titulo = "Tienda Virtual"
+      />
+      <h1>Lista de Productos</h1>
+      {productos.map(producto =>(
+        <Producto 
+          key = {producto.id}
+          producto = {producto}
+          productos = {productos}
+          carrito = {carrito}
+          agregarProducto = {agregarProducto}
+        />
+      ))}
+      <Carrito 
+        carrito = {carrito}
+        agregarProducto={agregarProducto}
       />
       <Footer
         fecha = {fecha}
